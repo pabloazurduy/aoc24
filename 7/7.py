74989c4e8e6b4f59f506f4f8624674507d8babd9
pabloazurduy <pablo.azurduy@ug.uchile.cl>
2024-12-07 18:59:21 +0100

    newfeature: added day 7

---
+++ 7/7.py
@@ -0,0 +1,30 @@
+from typing import List 
+
+
+def calib(nums:List[int], rt:int, target) -> bool:
+    
+    if len(nums)==0:
+        return rt==target
+    
+    if rt > target:
+        return False
+        
+    return (calib(nums[1:], rt=(rt + nums[0]), target=target) or 
+            calib(nums[1:], rt=(rt * nums[0]), target=target))
+    
+
+
+
+if __name__ == '__main__':
+    with open('7/data.txt', 'r') as file:
+        lines = file.read().splitlines() 
+    
+    rsum = 0
+    for line in lines:
+        line_s = line.split(' ')    
+        target = int(line_s[0].replace(':', ''))
+        nums = [int(n) for n in line_s[1:]]
+        
+        if calib(nums, rt=0, target=target):
+            rsum += target
+            print(target, rsum)
