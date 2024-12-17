0f1ecf98e63abb2730d4b20c79cc9fbebfdeba63
pabloazurduy <pablo.azurduy@ug.uchile.cl>
2024-12-17 18:05:27 +0100

    newfeature: added day 14

---
+++ 14/14.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Tuple, List
+import re
+
+import numpy as np
+
+@dataclass(repr=True)
+class Robot:
+    pos:Tuple[int,int]
+    sp:Tuple[int,int]
+
+    def move(self, shape:Tuple[int,int]):
+        nx = (self.pos[0] + self.sp[0])%shape[0]
+        ny = (self.pos[1] + self.sp[1])%shape[1]
+        self.pos = (nx, ny)
+
+@dataclass
+class Map:
+    robots:List[Robot]
+    shape:Tuple[int,int]
+    
+    @property
+    def to_array(self) -> np.ndarray:
+        mp = np.zeros(self.shape)
+        for rb in self.robots:
+            mp[rb.pos[0], rb.pos[1]] += 1
+        return mp.T
+    
+    def move(self, n:int =1) -> None:
+        for _ in range(n):
+            for rb in self.robots:
+                rb.move(self.shape)
+    @property
+    def safety_factor(self) -> int:
+        mp = self.to_array
+        lx = mp.shape[0]//2
+        ly = mp.shape[1]//2
+        sf = (mp[:lx, :ly].sum() * 
+              mp[lx+1:, ly+1:].sum() * 
+              mp[:lx, ly+1:].sum() * 
+              mp[lx+1:, :ly].sum())
+        return int(sf )
+
+if __name__ == '__main__':
+    with open('14/data.txt', 'r') as file:
+        lines = file.read().splitlines()
+    
+    robots:List[Robot] =[] 
+    for l in lines:
+        tups = re.findall(r'-?\d+,-?\d+', l)
+        pos = [int(i) for i in tups[0].split(',')]
+        speed = [int(i) for i in tups[1].split(',')]
+        rb = Robot(tuple(pos), tuple(speed))
+        robots.append(rb)
+        print(rb)
+
+    mp = Map(robots, shape=(101,103))
+    print(mp.to_array)
+    mp.move(100)
+    print(mp.to_array)
+    print(f'{mp.safety_factor = }')
